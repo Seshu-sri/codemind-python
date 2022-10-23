9da15ac8a93fd7a37a8b2aa0edaaeff2a30ff14a
Seshu-sri <100022250+Seshu-sri@users.noreply.github.com>
2022-10-23 11:49:48 +0530

    def self(num):     temp=num     while temp:         d=temp%10         temp=temp//10         if d==0 or num%d!=0:            return False     return True n=int(input()) m=int(input()) for i in range(n,m+1):     if self(i):         print(i,end=" ")

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,16 @@
+def self(num):
+    temp=num
+    while temp:
+        d=temp%10
+        temp=temp//10
+        if d==0 or num%d!=0:
+           return False
+    return True
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if self(i):
+        print(i,end=" ")
+        
+            
+        
